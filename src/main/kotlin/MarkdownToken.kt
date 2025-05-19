6409c659edb09f2aacb02fd3b2ff0f2a98d56305
zahid4kh <halilzahid@gmail.com>
2025-05-19 16:29:28 +0200

    new md tokens: image, banner, table

--- src/main/kotlin/MarkdownToken.kt
+++ src/main/kotlin/MarkdownToken.kt
@@ -2,7 +2,10 @@ sealed class MarkdownToken {
     data class Heading(val level: Int, val text: String) : MarkdownToken()
     data class Paragraph(val inlines: List<InlineToken>) : MarkdownToken()
     data class Bullet(val inlines: List<InlineToken>) : MarkdownToken()
-    data class CodeBlock(val text: String) : MarkdownToken()
+    data class CodeBlock(val language: String, val text: String) : MarkdownToken()
+    data class Image(val altText: String, val url: String) : MarkdownToken()
+    data class Banner(val type: BannerType, val inlines: List<InlineToken>) : MarkdownToken()
+    data class Table(val headers: List<String>, val rows: List<List<String>>) : MarkdownToken()
 }
 
 enum class BannerType {
