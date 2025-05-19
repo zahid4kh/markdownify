b56a5d542ba63119b352893a8f2f1aa08e894365
zahid4kh <halilzahid@gmail.com>
2025-05-19 14:29:54 +0200

    markdown token types

---
+++ src/main/kotlin/MarkdownToken.kt
@@ -0,0 +1,13 @@
+sealed class MarkdownToken {
+    data class Heading(val level: Int, val text: String) : MarkdownToken()
+    data class Paragraph(val inlines: List<InlineToken>) : MarkdownToken()
+    data class Bullet(val inlines: List<InlineToken>) : MarkdownToken()
+    data class CodeBlock(val text: String) : MarkdownToken()
+}
+
+sealed class InlineToken {
+    data class Text(val text: String) : InlineToken()
+    data class Bold(val text: String) : InlineToken()
+    data class Italic(val text: String) : InlineToken()
+    data class Code(val text: String) : InlineToken()
+}
