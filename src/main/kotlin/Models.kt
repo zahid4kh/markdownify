b0ad89e3fa7e9dfe3761dfbee2eb2c74fa34a28b
zahid4kh <halilzahid@gmail.com>
2025-06-11 12:01:54 +0200

    data models

--- src/main/kotlin/Models.kt
+++ src/main/kotlin/Models.kt
@@ -1,8 +1,26 @@
 import kotlinx.serialization.Serializable
 import java.io.File
 
-
 @Serializable
 data class AppSettings(
-    val darkMode: Boolean = false
+    val darkMode: Boolean = false,
+    val windowState: WindowState = WindowState()
 )
+
+@Serializable
+data class WindowState(
+    val width: Int = 800,
+    val height: Int = 600,
+    val positionX: Int = -1,
+    val positionY: Int = -1
+)
+
+data class OpenFile(
+    val file: File?,
+    val content: String,
+    val isModified: Boolean = false,
+    val title: String = "Untitled"
+) {
+    val displayTitle: String
+        get() = if (isModified) "$title*" else title
+}
